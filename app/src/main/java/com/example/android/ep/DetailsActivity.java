003ec1c7cb7663036db6019d6b35319000cca184
n <mapsandapps.dev@gmail.com>
2018-06-22 23:46:35 -0400

    Added DetailsActivity class

---
+++ app/src/main/java/com/example/android/ep/DetailsActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.ep;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    TextView mTitleTv, mDetailTv;
+    ImageView mImageIv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Details");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        mTitleTv = findViewById(R.id.titleTv);
+        mDetailTv = findViewById(R.id.descriptionTv);
+        mImageIv = findViewById(R.id.imageView);
+
+        byte[] bytes = getIntent().getByteArrayExtra("image");
+        String title = getIntent().getStringExtra("title");
+        String desc = getIntent().getStringExtra("description");
+        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+        mTitleTv.setText(title);
+        mDetailTv.setText(desc);
+        mImageIv.setImageBitmap(bmp);
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+}
