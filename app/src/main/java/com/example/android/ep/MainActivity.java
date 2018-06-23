cd4a61db45dbcd6ac14523bf375a66bd81197d19
n <mapsandapps.dev@gmail.com>
2018-06-22 23:48:38 -0400

    Added Details intent to MainActivity. Added OnClickListener to ViewHolder.

--- app/src/main/java/com/example/android/ep/MainActivity.java
+++ app/src/main/java/com/example/android/ep/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.android.ep;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,11 +11,16 @@ import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
  RecyclerView mRecyclerView;
@@ -44,6 +53,40 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     protected void populateViewHolder(ViewHolder holder, Model model, int position) {
                         holder.setDetails(getApplication(), model.getTitle(), model.getDescription(), model.getImage());
+                    }
+
+
+                    @Override
+                    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+                        ViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
+                        viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                TextView mTitleTv = view.findViewById(R.id.rTitleTv);
+                                TextView mDescTv = view.findViewById(R.id.rDescriptionTv);
+                                ImageView mImageView = view.findViewById(R.id.rImageView);
+
+                                String mTitle = mTitleTv.getText().toString();
+                                String mDesc = mDescTv.getText().toString();
+                                Drawable mDrawable = mImageView.getDrawable();
+                                Bitmap mBitmap = ((BitmapDrawable)mDrawable).getBitmap();
+
+                                Intent intent = new Intent(view.getContext(), DetailsActivity.class);
+                                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                                mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                                byte[] bytes = stream.toByteArray();
+                                intent.putExtra("image", bytes);
+                                intent.putExtra("title", mTitle);
+                                intent.putExtra("description", mDesc);
+                                startActivity(intent);
+                            }
+
+                            @Override
+                            public void onItemLongClick(View view, int position) {
+
+                            }
+                        });
+                        return viewHolder;
                     }
                 };
         mRecyclerView.setAdapter(firebaseRecyclerAdapter);
--- app/src/main/java/com/example/android/ep/ViewHolder.java
+++ app/src/main/java/com/example/android/ep/ViewHolder.java
@@ -15,6 +15,21 @@ public class ViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
 
         mView = itemView;
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mClickListener.onItemClick(view, getAdapterPosition());
+            }
+        });
+
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mClickListener.onItemLongClick(view, getAdapterPosition());
+                return true;
+            }
+        });
     }
 
     public void setDetails(Context ctx, String title, String description, String image){
@@ -24,5 +39,17 @@ public class ViewHolder extends RecyclerView.ViewHolder {
         mTitleTv.setText(title);
         mDetailTv.setText(description);
         Picasso.get().load(image).into(mImageIv);
+    }
+
+    private ViewHolder.ClickListener mClickListener;
+
+    public interface ClickListener{
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+
+    }
+
+    public void setOnClickListener(ViewHolder.ClickListener clickListener){
+        mClickListener = clickListener;
     }
 }
